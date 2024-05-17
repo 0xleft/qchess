f5389f64e90a3b9fc3babc44eb1e9994a83c608d
left <107749872+0xleft@users.noreply.github.com>
2024-05-17 23:19:42 +0100

    more

---
+++ web/src/components/chess/Chessboard.jsx
@@ -0,0 +1,99 @@
+import { Chess, SQUARES } from "chess.js";
+import { useEffect, useState } from "react";
+import Piece from "./Piece";
+import useMousePosition from "../hooks/useMousePosition";
+
+const Role = {
+	WHITE: 'w',
+	BLACK: 'b',
+	SPECTATOR: 's'
+};
+
+export default function Chessboard({ id }) {
+	const [boardState, setBoardState] = useState(new Chess()); // todo null before after fetch then set to new Chess(fen)
+	const [role, setRole] = useState(Role.WHITE); // todo default spectator
+	const [hoverSquare, setHoverSquare] = useState(null);
+
+	// const client = new WebSocket('ws://localhost:8080/ws');
+	// client.onopen = () => {
+	//     console.log('WebSocket connection established');
+	// };
+	// client.onmessage = (message) => {
+	//     const data = JSON.parse(message.data);
+	//     if (data.type === 'state') {
+	//     }
+	// };
+	const mousePosition = useMousePosition({ includeTouch: true });
+
+	useEffect(() => {
+		const handleMouseUp = () => {
+			setHoverSquare(null);
+		};
+	
+		if (typeof window !== 'undefined') {
+			window.addEventListener('mouseup', handleMouseUp);
+		}
+	
+		// Clean up the event listener when the component is unmounted
+		return () => {
+			if (typeof window !== 'undefined') {
+				window.removeEventListener('mouseup', handleMouseUp);
+			}
+		};
+	}, []);
+
+	return (
+		<>
+			<div className={"fixed transform " + (hoverSquare ? 'block' : 'hidden')} style={{ top: mousePosition.y, left: mousePosition.x, zIndex: 1000, pointerEvents: 'none', transform: 'translate(-50%, -50%)'
+			}}
+			>
+				{hoverSquare ? (
+					<Piece
+						type={boardState.get(hoverSquare).type}
+						color={boardState.get(hoverSquare).color}
+						width={50}
+						height={50}
+					/>
+				) : null}
+			</div>
+
+			{Array.from({ length: 8 }).map((_, i) => (
+				<div key={i} className="h-12 flex flex-row select-none">
+					{Array.from({ length: 8 }).map((_, j) => (
+						<div key={j} className={`square ${i % 2 === j % 2 ? 'bg-[#f0d9b5]' : 'bg-[#b58863]'} w-12 h-12`}
+							onMouseDown={() => {
+								if (role === Role.SPECTATOR) return;
+								if (boardState.get(SQUARES[8 * i + j])?.color !== role) return;
+
+								if (boardState.get(SQUARES[8 * i + j])) {
+									setHoverSquare(SQUARES[8 * i + j]);
+								}
+							}}
+
+							onMouseUp={() => {
+								if (hoverSquare) {
+									const moves = boardState.moves({ square: hoverSquare });
+									if (moves.some(move => move.includes(SQUARES[8 * i + j]))) {
+										boardState.move({ from: hoverSquare, to: SQUARES[8 * i + j] });
+										setBoardState(new Chess(boardState.fen()));
+									}
+									setHoverSquare(null);
+								}
+							}}
+						>
+							{boardState.get(SQUARES[8 * i + j]) && hoverSquare !== SQUARES[8 * i + j] ? (
+								<Piece
+									type={boardState.get(SQUARES[8 * i + j]).type}
+									color={boardState.get(SQUARES[8 * i + j]).color}
+									width={50}
+									height={50}
+									props={{ style: { pointerEvents: 'none', userSelect: 'none' } }}
+								/>
+							) : null}
+						</div>
+					))}
+				</div>
+			))}
+		</>
+	);
+};
---
+++ web/src/components/chess/Piece.jsx
@@ -0,0 +1,14 @@
+import { Chess } from "chess.js";
+import Image from "next/image";
+import { useState } from "react";
+
+
+
+export default function Piece({ props, type, color, width, height }) {
+
+	return (
+        <Image {...props} 
+        draggable="false" src={`/pieces/${type}-${color}.svg`} alt={`${color} ${type}`} width={width} height={height} className="pointer-events-none"
+        />
+    );
+};
