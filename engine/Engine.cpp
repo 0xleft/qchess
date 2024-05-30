f6d7dbfe549b0882b184638dbc0cf71e1ff2f7a8
left <107749872+0xleft@users.noreply.github.com>
2024-05-30 22:17:36 +0100

    testing web assembly

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -1,17 +1,34 @@
-#include "../ws/include/chess.hpp"
+//#include "../ws/include/chess.hpp"
 #include "Engine.h"
+#include <math.h>
+
+#ifdef __EMSCRIPTEN__
+  #include <emscripten.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 int main() {
     
-    chess::Board board = chess::Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
-    chess::Movelist moves;
-    chess::movegen::legalmoves(moves, board);
-
-    for (const auto &move : moves) {
-        std::cout << chess::uci::moveToUci(move) << std::endl;
-    }
+    // chess::Board board = chess::Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+// 
+    // chess::Movelist moves;
+    // chess::movegen::legalmoves(moves, board);
+// 
+    // for (const auto &move : moves) {
+    //     std::cout << chess::uci::moveToUci(move) << std::endl;
+    // }
 
 
     return 0;
 }
+
+int int_sqrt(int x) {
+  return sqrt(x);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ web/src/pages/test.jsx
@@ -0,0 +1,26 @@
+import Script from "next/script";
+import { useEffect, useRef } from "react";
+
+export default function Test() {
+
+    const int_sqrt = useRef(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            if(typeof window.Engine === 'function' && WebAssembly.current === undefined){
+                window.Engine()
+                    .then(wasm => {
+                        console.log("got WASM!");
+                        WebAssembly.current = wasm;
+                        int_sqrt.current = WebAssembly.current.cwrap("int_sqrt", "number", ["number"]);
+                    });
+            }
+        }, 1000);
+    }, []);    
+
+    return (
+		<>
+            <Script src="/engine.js" strategy='beforeInteractive'/>
+        </>
+	);
+}
