233885d8f13a6a9fae8eff1b14f8dee15d4bf0d3
left <107749872+0xleft@users.noreply.github.com>
2024-06-01 12:09:11 +0100

    Update index.jsx

--- web/src/pages/analysis/index.jsx
+++ web/src/pages/analysis/index.jsx
@@ -1,4 +1,8 @@
+import Chessboard from '@/components/chess/AnalysisChessboard';
 import * as React from 'react';
 
 export default function AnalysisIndex() {
+    return (
+        <Chessboard />
+    )
 }
